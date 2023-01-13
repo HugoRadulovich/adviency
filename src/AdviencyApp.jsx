4fd231b56e9a8fe04fb2fe2986502bbdf3abe7e6
Hugo Radulovich <hcradulovich@gmail.com>
2023-01-12 21:03:25 -0300

    avance de proyecto

---
+++ src/AdviencyApp.jsx
@@ -0,0 +1,30 @@
+
+
+import React from 'react'
+
+export const AdviencyApp = () => {
+    
+    
+    
+    return (
+        
+        <>
+            <div className="container">
+                <div className="box">
+                    <h1>Regalos</h1>
+                    <input/>
+                    
+                    <button>Agregar</button>
+                    <ul>
+                        <li>Bicicleta</li>
+                        <li>Auto</li>
+                        <li>Pileta</li>
+                    </ul>
+
+                </div>
+            </div>
+    
+        </>
+
+    )
+}
---
+++ src/hooks/useForm.js
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+
+export const useForm = (initalForm = {}) => {
+
+    const [formState, setFormState] = useState(initalForm);
+
+    const onIpuntChange = ({target}) => {
+        const {name, value} = target;
+        setFormState({
+            ...formState,
+            [name]: value,
+        })
+    };
+
+    const onResetForm = () => {
+        setFormState(initalForm)
+    } 
+
+    return {
+        ...formState,
+        formState,
+        onIpuntChange,
+        onResetForm,
+    }
+}   
