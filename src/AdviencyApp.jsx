08b28d6215f9f91f6b3ea343a3549d067d726f5f
Hugo Radulovich <hcradulovich@gmail.com>
2023-01-16 21:50:35 -0300

    avance de proyecto

--- src/AdviencyApp.jsx
+++ src/AdviencyApp.jsx
@@ -1,30 +1,63 @@
+import { useState } from "react"
+import { useForm } from "./hooks/useForm"
 
 
-import React from 'react'
 
 export const AdviencyApp = () => {
-    
-    
-    
+
+    const {description,onIpuntChange, onResetForm} = useForm({
+        description:'',
+    })
+
+    const [regalos, setRegalos] = useState(['Bicicleta','Auto','Pelota'])
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        if(description.length <= 1) return;
+
+        const newRegalo = {
+            id: new Date().getTime(),
+            description: description,
+            done: false,
+        }
+
+        setRegalos(...regalos,newRegalo);
+        onResetForm();
+
+    }
+
     return (
-        
-        <>
-            <div className="container">
-                <div className="box">
-                    <h1>Regalos</h1>
-                    <input/>
-                    
-                    <button>Agregar</button>
-                    <ul>
-                        <li>Bicicleta</li>
-                        <li>Auto</li>
-                        <li>Pileta</li>
-                    </ul>
+    <div className="aa">
+        <div className="container">
+        <div className="box">
+            <h2>Regalos</h2>
+            <form onSubmit={onFormSubmit}>
+                <input
+                type="text"
+                className=""
+                placeholder="Ingrese regalo"
+                name="description"
+                value= {description}
+                onChange = {onIpuntChange}
+                />
+                <button>Agregar</button>
+            </form>
+           
+            <ul>
+                {
+                    regalos.map(regalo =>
 
-                </div>
-            </div>
-    
-        </>
-
+                        <li key={regalo.id}>{regalo.description}</li> 
+                        )
+                }
+                {/* <li>Bicicleta</li>
+                <li>Auto control remoto</li>
+                <li>Pelota de futbol</li> */}
+            </ul>
+        </div>
+       </div>
+    </div>
+       
     )
 }
