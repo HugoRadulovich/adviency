a3cb4d85d920c1ac6dcda6f6a51ca3362435d731
Hugo Radulovich <hcradulovich@gmail.com>
2023-02-08 21:06:12 -0300

    reinicio de proyecto

--- src/AdviencyApp.jsx
+++ src/AdviencyApp.jsx
@@ -1,10 +1,4 @@
 
-import React, { useEffect, useReducer } from 'react'
-import { Modal } from './components/Modal'
-import { RegaloAdd } from './components/RegaloAdd'
-import { RegaloList } from './components/RegaloList'
-import { regalosReducer } from './helpers/regalosReducer'
-
 
 
 const initialState = [{
@@ -21,65 +15,23 @@ const initialState = [{
 }
 ]
 
-const init = () => {
-    return JSON.parse(localStorage.getItem('regalos')) || []
-}
 
 export const AdviencyApp = () => {
 
-    const [regalos, dispatch] = useReducer(regalosReducer,initialState,init )
-
-
-    useEffect(() => {
-        localStorage.setItem('regalos',JSON.stringify(regalos))
-    }, [regalos])
-    
-
-
-    const handleNewRegalo = (regalo) => {
-        const action = {
-            type: '[REGALO] Add Regalo',
-            payload: regalo,
-        }
-
-        dispatch(action);
-    }
-
-
-    const handleDeleteRegalo = (id) => {
-
-        const action = {
-            type: '[REGALO] Remove Regalo',
-            payload: id,
-        }
-        dispatch(action)
-
-    }
-
-    const handleDeleteTodo = () => {
-        const action = {
-            type: '[REGALO] Remove Total',
-            
-        }
-        dispatch(action)
-    }
-
-    
 
     
 
     return (
         <>
-            <div className="aa">
-                <div className="container">
-                    <div className="box">
-                        <h2>Regalos</h2>
-                        <Modal handleNewRegalo={handleNewRegalo} regalos={regalos}/>
-                        <RegaloList regalos={regalos} onDelete={handleDeleteRegalo} />
-                        <button onClick={handleDeleteTodo}>Borrar Todo</button>
+            
+                
+                    <div className="container border d-flex justify-content-center align-items-center">
+                    <div className="card">
+                        <div className="card-body"></div>
                     </div>
-                </div>
-            </div>
+                    </div>
+                
+
         </>
     )
 }
